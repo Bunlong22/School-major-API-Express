3543b65ce264d5175248272b506a76355feef187
bunlong22 <suonbunlong93@gmail.com>
2023-06-13 22:56:05 +0700

    added new end point

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Get all the school Major!");
 });
+app.get('/api/majors', (req, res) => {
+    res.send([
+        'Name: E-Commerce',
+        'Name: Computer Science',
+        'Name: Telecom and Networking'
+    ])
+})
 app.listen(3003, () => console.log("Listening on Port 3003..."));
