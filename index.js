debf48db3466f2152b0adde18e8a30bb33eeabca
bunlong22 <suonbunlong93@gmail.com>
2023-06-13 22:48:12 +0700

    simple get method, display some text back

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const express = require("express");
 const app = express();
+app.get("/", (req, res) => {
+  res.send("Get all the school Major!");
+});
+app.listen(3003, () => console.log("Listening on Port 3003..."));
